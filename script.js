6ebebabfbf6d6bb51f5687dd22b81188d118768c
Tom.v.d.M <tom.v.d.muijsenberg@gmail.com>
2020-09-03 15:05:02 +0200

    added text to speech

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+function scrollToId(toFindElId) {
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = "'t Waepen van Veere is een hotel/restaurant in Veere en is momenteel mijn huidige werk, ik begon hier in 2017 als afwasser en doe tegenwoordig kun je mij ook vinden achter de bar.";
+    window.speechSynthesis.speak(msg);
+    document.getElementById(toFindElId).scrollIntoView({ behavior: "smooth" });
+}
